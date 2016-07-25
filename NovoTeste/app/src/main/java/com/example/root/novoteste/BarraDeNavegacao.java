a3bb57d03c7a57a6d8afe61e0fe3f5c8e492fad8
matheus7589 <matheusmachado@uft.edu.br>
2016-07-25 17:47:00 -0300

    sei la o que eu fiz

---
+++ NovoTeste/app/src/main/java/com/example/root/novoteste/BarraDeNavegacao.java
@@ -0,0 +1,158 @@
+package com.example.root.novoteste;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class BarraDeNavegacao extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+
+    //private SupportMapFragment mapFragment;
+    //private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barra_de_navegacao);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //MENU LATERAL
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        //MENU LATERAL
+
+        //GOOGLE MAP
+        //seta o menu lateral para nao ficar em baixo do google map
+        //GoogleMapOptions options = new GoogleMapOptions();
+        //options.zOrderOnTop(true);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        //mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        //map = mapFragment.getMapAsync(this);
+
+        /*mapFragment = SupportMapFragment.newInstance(options);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.llContainer, mapFragment);
+        ft.commit();*/
+
+        //configMap();
+
+
+        //GOOGLE MAP
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        //this.map = googleMap;
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(-10.1689, -48.3317)).title("Palmas"));
+    }
+
+//    public void configMap(){
+//        map = mapFragment.getMapAsync(OnMapReadyCallback this);
+//
+//        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+//        map.setMyLocationEnabled(true);
+//
+//        LatLng latLng = new LatLng(-10.1689, -48.3317);
+//
+//        CameraPosition cameraPosition = new CameraPosition.Builder().target(latLng).zoom(13).build();
+//        CameraUpdate update = CameraUpdateFactory.newCameraPosition(cameraPosition);
+//
+//        map.animateCamera(update);
+//    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.barra_de_navegacao, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            Intent avaliacao = new Intent(this, AvaliacaoSensitiva.class);
+            startActivity(avaliacao);
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+}
