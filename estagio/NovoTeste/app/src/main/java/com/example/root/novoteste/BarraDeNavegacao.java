75a00c96e923e2d100a0cdc3af139bbe26474586
matheus7589 <matheus-machadovilarino@hotmail.com>
2016-07-25 11:49:24 -0300

    google maps

--- estagio/NovoTeste/app/src/main/java/com/example/root/novoteste/BarraDeNavegacao.java
+++ estagio/NovoTeste/app/src/main/java/com/example/root/novoteste/BarraDeNavegacao.java
@@ -1,9 +1,12 @@
 package com.example.root.novoteste;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
+import android.view.SubMenu;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,9 +16,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class BarraDeNavegacao extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private SupportMapFragment mapFragment;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +41,7 @@ public class BarraDeNavegacao extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //MENU LATERAL
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -32,6 +50,56 @@ public class BarraDeNavegacao extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        //MENU LATERAL
+
+        //GOOGLE MAP
+        //seta o menu lateral para nao ficar em baixo do google map
+        GoogleMapOptions options = new GoogleMapOptions();
+        options.zOrderOnTop(true);
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        map = mapFragment.getMap();
+
+        /*mapFragment = SupportMapFragment.newInstance(options);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.llContainer, mapFragment);
+        ft.commit();*/
+
+        configMap();
+
+
+        //GOOGLE MAP
+    }
+
+    //é necessário usar isso porque está trabalhando com fragment via API
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public void configMap() {
+        map = mapFragment.getMap();
+
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        if (checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        map.setMyLocationEnabled(true);
+
+        LatLng latLng = new LatLng(-10.1689, -48.3317);
+
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(latLng).zoom(13).build();
+        CameraUpdate update = CameraUpdateFactory.newCameraPosition(cameraPosition);
+
+        map.animateCamera(update);
     }
 
     @Override
