5b026db29eaab53d1dcc0b67a7ef91e3c2ab90a3
matheus7589 <matheusmachado@uft.edu.br>
2016-08-01 17:59:48 -0300

    nao consegui adicionar o fragment

---
+++ NovoTeste/app/src/main/java/com/example/root/novoteste/Fragmento.java
@@ -0,0 +1,21 @@
+package com.example.root.novoteste;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by root on 01/08/2016.
+ */
+public class Fragmento extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Infla o Layout pra esse fragmento
+        return inflater.inflate(R.layout.activity_avaliacao_sensitiva, container, false);
+    }
+
+}
