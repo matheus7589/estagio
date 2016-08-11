3d180ae6d5c9ceb1ef95cff1d9c14d23b79510b5
matheus7589 <matheusmachado@uft.edu.br>
2016-08-11 12:16:13 -0300

    função add markers adicionada

--- NovoTeste/app/src/main/java/com/example/root/novoteste/Maps.java
+++ NovoTeste/app/src/main/java/com/example/root/novoteste/Maps.java
@@ -6,18 +6,29 @@ import android.os.Bundle;
 import android.support.multidex.MultiDex;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Maps extends AppCompatActivity implements OnMapReadyCallback {
+public class Maps extends AppCompatActivity implements OnMapReadyCallback, OnMapClickListener, OnMapLongClickListener {
+
+    GoogleMap mapa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +39,40 @@ public class Maps extends AppCompatActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+    }
+
+
+    @Override
+    public void onMapClick(LatLng point) {
+
+//        mapa.addMarker(new MarkerOptions().position(point).title(
+//                point.toString()));
+//
+//        Toast.makeText(getApplicationContext(),
+//                "New marker added@" + point.toString(), Toast.LENGTH_LONG)
+//                .show();
+
     }
 
     @Override
+    public void onMapLongClick(LatLng point) {
+        mapa.addMarker(new MarkerOptions()
+                .position(point)
+                .title("Você está aqui!")
+                .draggable(true));
+
+        Toast.makeText(getApplicationContext(),
+                "Novo marcador adicionado: " + point.toString(), Toast.LENGTH_LONG)
+                .show();
+    }
+
+
+
+    @Override
     public void onMapReady(GoogleMap map) {
+        mapa = map;
 
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         map.addMarker(new MarkerOptions()
@@ -56,7 +97,12 @@ public class Maps extends AppCompatActivity implements OnMapReadyCallback {
 
         map.animateCamera(update);
 
+        mapa.setOnMapClickListener(this);
+        mapa.setOnMapLongClickListener(this);
+
     }
+
+
 
     //    protected void attachBaseContext(Context base) {
 //        super.attachBaseContext(base);
