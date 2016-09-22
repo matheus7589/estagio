cdbdff9dcd62ee02e9b14c6c0db283b11d404ee9
matheus7589 <matheusmachado@uft.edu.br>
2016-09-22 11:54:03 -0300

    Cadastro Domiciliar

--- NovoTeste/app/src/main/java/com/example/root/novoteste/Cadastro_Domi1.java
+++ NovoTeste/app/src/main/java/com/example/root/novoteste/Cadastro_Domi1.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by root on 20/09/2016.
@@ -23,5 +25,20 @@ public class Cadastro_Domi1 extends Fragment {
                              Bundle savedInstanceState) {
         // Infla o Layout pra esse fragmento
         return inflater.inflate(R.layout.cadastro_domiciliar1, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Spinner spinner = (Spinner) view.findViewById(R.id.spinner2);
+        // Cria um  ArrayAdapter usando um array de string e um spinner padrão
+        // USAR getActivity().getApplicationContext() CASO getContext() NÃO FUNCIONE
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.UF, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
